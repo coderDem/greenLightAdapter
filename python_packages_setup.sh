e2d48e401dd51d0ca73e422fceb0fc662a517552
coderDem <ivandemin@hotmail.de>
2020-09-29 13:37:15 +0200

    Update python_packages_setup.sh

--- python_packages_setup.sh
+++ python_packages_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt -y install python3-pip
 apt -y install python3-psycopg2
-pip3 install -r /usr/local/bin/scheduLight/python_packages.txt
+pip3 install -r /usr/local/bin/greenLightAdapter/python_packages.txt
